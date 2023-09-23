29078c5870502d9e45c0bee03e81499cf5192c23
jcusuga <camilourodriguez@gmail.com>
2023-09-23 13:24:59 -0500

    "cambie la ruta"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'todo', loadChildren: () => import('./todo/todo.module').then(m => m.TodoModule) }, { path: 'post', loadChildren: () => import('./post/post.module').then(m => m.PostModule) }, { path: 'usuario', loadChildren: () => import('./usuario/usuario.module').then(m => m.UsuarioModule) }, { path: 'comentarios', loadChildren: () => import('./comentarios/comentarios.module').then(m => m.ComentariosModule) }];
+const routes: Routes = [{ path: 'todo', loadChildren: () => import('./todo/todo.module').then(m => m.TodoModule) }, 
+{ path: 'post', loadChildren: () => import('./post/post.module').then(m => m.PostModule) }, 
+{ path: 'usuario', loadChildren: () => import('./usuario/usuario.module').then(m => m.UsuarioModule) }, 
+{ path: 'comentarios', loadChildren: () => import('./comentarios/comentarios.module').then(m => m.ComentariosModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
